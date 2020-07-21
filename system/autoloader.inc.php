5090731835c834501fdd0e688ca8dd7d30a637c0
unknown <velimir.mueller@googlemail.com>
2020-07-22 00:44:04 +0200

    minor changes latenight before sleep

--- system/autoloader.inc.php
+++ system/autoloader.inc.php
@@ -2,14 +2,14 @@
 //php/autoloader.inc.php
 
 
+
 spl_autoload_register(function($class_name) {
 
     //funktionsvariable; array in foreach loop um alle classen automatisch zu laden,
     // wenn man neue Ordner hinzufügen möchte, muss man einen Eintrag im array erweitern
     //und die Klassen werden automatisch in den autoloader geladen.
-
     $folderstructure = array('system','helpers','model','view', 'controller');
-
+    
     foreach($folderstructure as $folder){
         if(file_exists($folder.'/class_'.$class_name.'.inc.php' )){
             require_once($folder."/class_".$class_name.".inc.php");
